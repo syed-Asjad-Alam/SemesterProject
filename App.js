58fe7edae259b71cf497ea71f1e61c3369d9d8c9
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-12-28 22:03:16 +0500

    Created Admin Login Screen(Just UI/Front-end

--- App.js
+++ App.js
@@ -1,70 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
+import { LoginScreen } from './src/screens'
 
 
 
-const FIREBASE_API_ENDPOINT = 'https://semesterproject-7a2e1-default-rtdb.firebaseio.com/'
 
-export default function App() {
-  const postData = () => {
-    var requestOptions = {
-      method: 'POST',
-      body: JSON.stringify({
-        employeename: 'New Employee',
-        taskdesc: 'New Task',
-      }),
-    };
-
-    fetch(`${FIREBASE_API_ENDPOINT}/tasks.json`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.log('error', error));
-  };
-
-  const getData = async () => {
-    const response = await fetch(`${FIREBASE_API_ENDPOINT}/tasks.json`);
-    const data = await response.json();
-    console.log(data);
-  };
-
-  const deleteData = () => {
-    const id = '-MrWvChDCFKoDz4hiiVp';
-    var requestOptions = {
-      method: 'DELETE',
-    };
-
-    fetch(`${FIREBASE_API_ENDPOINT}/tasks/${id}.json`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => console.log('Delete Response:', result))
-      .catch((error) => console.log('error', error));
-  };
-
-  const updateData = () => {
-    const id = '-Mra4c0ncxW5hz5GkRNJ';
-    var requestOptions = {
-      method: 'PATCH',
-      body: JSON.stringify({
-        username: 'UserName',
-        password: 'Password',
-        employeename: 'Update New Employee'
-      }),
-    };
-
-    fetch(`${FIREBASE_API_ENDPOINT}/tasks/${id}.json`, requestOptions)
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.log('error', error));
-  };
-
+const App =() => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>Firebase</Text>
-      <Button title="Firebase Post Data" onPress={postData} />
-      <Button title="Firebase Get Data" onPress={getData} />
-      <Button title="Firebase Delete Data" onPress={deleteData} />
-      <Button title="Firebase Update Data" onPress={updateData} />
-    </View>
-  );
+    <LoginScreen />
+  )
 }
 
 const styles = StyleSheet.create({
@@ -76,4 +20,6 @@ const styles = StyleSheet.create({
   },
 });
 
+
+export default App
 
