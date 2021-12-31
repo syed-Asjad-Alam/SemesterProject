3426d39a348298468500176f810bc7074d9cf211
Syed Asjad Alam <asjadakmal1@gmail.com>
2021-12-31 16:54:53 +0500

    added styling to HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, Text } from "react-native"
+import {Image, View, TouchableOpacity, Text, StyleSheet } from "react-native"
 
 
 
@@ -8,31 +8,117 @@ const HomeScreen = ({navigation}) => {
 
     return (
         <View>
-            <Text>Welcome back</Text>
-            <View>
-                <TouchableOpacity onPress={() =>navigation.navigate('UsersScreen')}>
-                    <Text>Users</Text>
+            <Text style = {styles.title}>Welcome back</Text>
+            <View style={styles.buttonView}>
+                <TouchableOpacity style={styles.button} onPress={() =>navigation.navigate('UsersScreen')}>
+                    <Text style={styles.upperText}>Users</Text>
+                    <Text style={styles.lowerText}>View Users</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() =>navigation.navigate('ProductsScreen')}>
-                    <Text>Products</Text>
-                </TouchableOpacity>
-            </View>
-            <View>
-                <TouchableOpacity onPress={() =>navigation.navigate('ReportsScreen')}>
-                    <Text>Reports</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() =>navigation.navigate('ReviewsScreen')}>
-                    <Text>Reviews</Text>
+                <TouchableOpacity style={styles.button} onPress={() =>navigation.navigate('ProductsScreen')}>
+                    <Text style={styles.upperText}>Products</Text>
+                    <Text style={styles.lowerText}>View Products</Text>
                 </TouchableOpacity>
             </View>
-            <View>
-                <TouchableOpacity onPress={() =>navigation.navigate('CategoriesScreen')}>
-                    <Text>Manage Categories</Text>
+            <View style={styles.buttonView}>
+                <TouchableOpacity style={styles.button} onPress={() =>navigation.navigate('ReportsScreen')}>
+                    <Text style={styles.upperText}>Reports</Text>
+                    <Text style={styles.lowerText}>View Reports</Text>
                 </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() =>navigation.navigate('ReviewsScreen')}>
+                    <Text style={styles.upperText}>Reviews</Text>
+                    <Text style={styles.lowerText}>View Reviews</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.singleButtonView}>
+                <TouchableOpacity style={styles.singleButton} onPress={() =>navigation.navigate('CategoriesScreen')}>
+                    <Text style={styles.upperText}>Categories</Text>
+                    <Text style={styles.lowerText}> Manage Categories</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.bottomTab}>
+                <TouchableOpacity activeOpacity={0.8} onPress={() =>navigation.navigate('CategoriesScreen')}>
+                <Image style={styles.mainButton}
+                source={require('../../assets/MC1.png')}
+            />
+            </TouchableOpacity>
             </View>
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontWeight: 'bold',
+        fontSize:24,
+        margin: 10,
+        padding:10,
+        paddingBottom:3,
+        marginBottom: 3,
+        color:'#788eec'
+
+    },
+    buttonView: {
+        margin:10,
+        padding:10,
+        flexDirection:'row',
+        
+
+    },
+    button: {
+        borderColor:'#788eec',
+        borderWidth:2,
+        borderRadius:10,
+        margin:10,
+        width:140
+    },
+    upperText: {
+        backgroundColor:'#788eec',
+        color:'white',
+        fontWeight:'bold',
+        fontSize:20,
+        padding:13,
+        justifyContent:'flex-start',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5
+    },
+    lowerText: {
+        backgroundColor:'white',
+        padding:13,
+        justifyContent:'center',
+        fontSize:16,
+        fontWeight:'bold',
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        color:'#788eec'
+    },
+    singleButtonView : {
+        margin:10,
+        padding:10
+    },
+    singleButton:{
+        borderColor:'#788eec',
+        borderWidth:2,
+        borderRadius:10,
+        margin:10
+    },
+    mainButton: {
+        width:70,
+        height:70,
+        alignSelf:'center',
+        position:'relative',
+        bottom:20,
+
+
+    },
+    bottomTab : {
+        backgroundColor:'#788eec',
+        position:'relative',
+        top:42,
+        flex:0
+
+    }
+
+})
 
 export default HomeScreen
